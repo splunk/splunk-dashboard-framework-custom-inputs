3c413170981962d130448d95069db40b1ab321a5
Caius Branda <cbranda@splunk.com>
2022-05-11 10:58:05 -0400

    add empty input in preset and render it in dashboard

---
+++ packages/my-splunk-app/src/main/webapp/pages/start/FlightWidget.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FlightWidget = () => {
+    return <div>Flight Settings</div>;
+};
--- packages/my-splunk-app/src/main/webapp/pages/start/index.jsx
+++ packages/my-splunk-app/src/main/webapp/pages/start/index.jsx
@@ -8,20 +8,26 @@ import DashboardCore from '@splunk/dashboard-core';
 import { DashboardContextProvider } from '@splunk/dashboard-context';
 import EnterprisePreset from '@splunk/dashboard-presets/EnterprisePreset';
 
+import { FlightWidget } from './FlightWidget';
+
 // Dashboard Definition Files
 import attemptOne from './attempt_one.json';
+import attemptTwo from './attempt_two.json';
 
 const themeProviderSettings = getThemeOptions(defaultTheme() || 'enterprise');
+
+const preset = {
+    ...EnterprisePreset,
+    inputs: {
+        ...EnterprisePreset.inputs,
+        'input.flightwidget': FlightWidget,
+    },
+};
 
 layout(
     <SplunkThemeProvider {...themeProviderSettings}>
         <DashboardContextProvider>
-            <DashboardCore
-                width="100%"
-                height="100%"
-                preset={EnterprisePreset}
-                definition={attemptOne}
-            />
+            <DashboardCore width="100%" height="100%" preset={preset} definition={attemptTwo} />
         </DashboardContextProvider>
     </SplunkThemeProvider>
 );
