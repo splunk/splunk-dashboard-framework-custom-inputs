a8f996b6d064626ddd54867744602813e973c55a
Caius Branda <cbranda@splunk.com>
2022-05-12 10:00:00 -0400

    link input values to dashboard tokens

--- packages/my-splunk-app/src/main/webapp/pages/start/FlightWidget.jsx
+++ packages/my-splunk-app/src/main/webapp/pages/start/FlightWidget.jsx
@@ -9,7 +9,32 @@ import Slider from '@splunk/react-ui/Slider';
 
 const menuStyle = { padding: 20, width: 300 };
 
-const FlightWidget = () => {
+const toValue = ({ origin, destination, priceMax, tripType } = {}) => {
+    if (!origin || !destination || priceMax == null || !tripType) {
+        return '';
+    }
+    return `${origin},${destination},${tripType},${priceMax}`;
+};
+
+const parseValue = (value) => {
+    if (typeof value !== 'string') {
+        return {};
+    }
+    const values = value.split(',');
+
+    if (values.length < 4) {
+        return {};
+    }
+
+    return {
+        origin: values[0].trim(),
+        destination: values[1].trim(),
+        tripType: values[2].trim(),
+        priceMax: Number(values[3].trim()),
+    };
+};
+
+const FlightWidget = ({ onValueChange, value }) => {
     const [open, setOpen] = useState(false);
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
@@ -45,6 +70,15 @@ const FlightWidget = () => {
     const handleRequestOpen = useCallback(() => {
         setOpen(true);
     }, []);
+
+    const handleAppy = useCallback(
+        (e) => {
+            const val = toValue({ origin, destination, priceMax, tripType });
+            setOpen(false);
+            onValueChange(e, val);
+        },
+        [destination, onValueChange, origin, priceMax, tripType]
+    );
 
     return (
         <Dropdown
@@ -88,9 +122,27 @@ const FlightWidget = () => {
                         value={priceMax}
                     />
                 </ControlGroup>
+                <Button inline={false} label="Apply" appearance="primary" onClick={handleAppy} />
             </div>
         </Dropdown>
     );
+};
+
+// token is "flight" or whatever value it gets in the definition
+// value is comma separated string of values that we save in handleApply
+FlightWidget.valueToTokens = (value, { token }) => {
+    if (!token) {
+        return {};
+    }
+    // need to parse 'origin,destination,tripType,priceMax` into 4 variables
+    const { origin, destination, tripType, priceMax } = parseValue(value);
+
+    return {
+        [`${token}.origin`]: origin,
+        [`${token}.destination`]: destination,
+        [`${token}.tripType`]: tripType,
+        [`${token}.priceMax`]: priceMax == null ? undefined : `${priceMax}`,
+    };
 };
 
 FlightWidget.propTypes = {
