9262195f279bb7921bbd844d091a0cbb6ccbdac8
Caius Branda <cbranda@splunk.com>
2023-07-13 09:04:39 -0400

    Upgrade dashboard framework to 26.0.0

--- packages/my-splunk-app/src/main/webapp/pages/start/index.jsx
+++ packages/my-splunk-app/src/main/webapp/pages/start/index.jsx
@@ -4,7 +4,7 @@ import layout from '@splunk/react-page';
 import { SplunkThemeProvider } from '@splunk/themes';
 
 import { defaultTheme, getThemeOptions } from '@splunk/splunk-utils/themes';
-import DashboardCore from '@splunk/dashboard-core';
+import { DashboardCore } from '@splunk/dashboard-core';
 import { DashboardContextProvider } from '@splunk/dashboard-context';
 import EnterprisePreset from '@splunk/dashboard-presets/EnterprisePreset';
 
@@ -31,8 +31,8 @@ const preset = {
 
 layout(
     <SplunkThemeProvider {...themeProviderSettings}>
-        <DashboardContextProvider>
-            <DashboardCore width="100%" height="100%" preset={preset} definition={attemptTwo} />
+        <DashboardContextProvider preset={preset} initialDefinition={attemptTwo}>
+            <DashboardCore width="100%" height="100%" />
         </DashboardContextProvider>
     </SplunkThemeProvider>
 );
